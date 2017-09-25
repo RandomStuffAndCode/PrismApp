7e66a2ceddaa229ab01f35bc29f36a1898e94d19
Mikkel Jensen <mgj@artm.dk>
2017-09-25 15:20:20 +0200

    initial

---
+++ PrismApp/PrismApp/Views/BPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismApp.Views
+{
+    public partial class BPage : ContentPage
+    {
+        public BPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
